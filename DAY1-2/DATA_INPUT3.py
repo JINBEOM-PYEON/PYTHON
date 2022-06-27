501fbd7859862c74ef33074d02e4500dc6d11723
편진범 <hapy1122@naver.com>
2022-06-27 04:49:08 +0000

    add

---
+++ DAY1-2/DATA_INPUT3.py
@@ -0,0 +1,11 @@
+# DAY1 - 입력과 출력
+a = input()
+print(a, 3 * ("\n"))
+print(a, a, a)
+b, c = input('문자 2개를 입력해주세요 : ').split()
+print(b + c + "\n")
+print(b, "\n" + c)
+d, e = input('문자 2개를 입력해주세요, 콤마 구분 : ').split(',')
+print(d, e)
+f, g = input('문자 2개를 입력해주세요, 콤마 구분, 1개 제한 : ').split(',', 1) 
+print(f, g, sep=",")
